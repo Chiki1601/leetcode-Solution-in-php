39cc5396db0604d90946b21c5fe5d174680aa2e1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-11 12:04:50 +0530

    Update 2843.php

--- 2501-3000/2843.php
+++ 2501-3000/2843.php
@@ -1,13 +1,38 @@
-function countSymmetricIntegers($low, $high) {
-    $cnt = 0;
-    for ($i = $low; $i <= $high; $i++) {
-        $s = strval($i);
-        $n = strlen($s);
-        if ($n % 2 == 0) {
-            $sum1 = array_sum(str_split(substr($s, 0, $n / 2)));
-            $sum2 = array_sum(str_split(substr($s, $n / 2)));
-            if ($sum1 == $sum2) $cnt++;
+class Solution {
+
+    /**
+     * @param Integer $low
+     * @param Integer $high
+     * @return Integer
+     */
+    function countSymmetricIntegers($low, $high) {
+        $result = 0;
+
+        for($i = $low; $i <= $high; $i++) {
+            $n = (string)$i;
+            $ln = strlen($n);
+
+            if (($ln % 2 !== 0)) {
+                continue;
+            }
+
+            $left = 0;
+            $right = 0;
+
+            $l = 0;
+            $r = $ln - 1;
+
+            while($l < $r) {
+                $left += $n[$l];
+                $right += $n[$r];
+
+                $l++;
+                $r--;
+            }
+        
+            $result += $left === $right? 1: 0;
         }
+
+        return $result;
     }
-    return $cnt;
 }
